0f768e3ea6fb3331330b7020ca528cd65aa66daf
Morgan Ferreira Lopes <wakkaauron@gmail.com>
2025-06-13 14:28:23 +0200

    Initial commite

---
+++ src/main/java/com/p6FullStack/Projet6FullStackApplication.java
@@ -0,0 +1,13 @@
+package com.p6FullStack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet6FullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet6FullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p6FullStack/Projet6FullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p6FullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projet6FullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
